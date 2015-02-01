43bb327442929447ab961c5485479946755d90b8
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2015-02-01 19:21:43 +0200

    So ugly way for trying to do XON/XOFF on application layer

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 module Main where
 
+import Control.Monad (when)
 import Data.Binary.Put
 import qualified Data.ByteString.Lazy as B
 import System.Environment
@@ -10,11 +11,30 @@ import EscPos
 import Serial
 import Loader
 
+main :: IO ()
 main = do
   [printerDev, imagePath] <- getArgs
-  Serial{..} <- openSerialOutRaw printerDev 19200
+  s@Serial{..} <- openSerialOutRaw printerDev 19200
   image <- loadMonochrome imagePath
-  B.hPut handle $ runPut $ putImage image
+  putXonXoff s $ runPut $ putImage image
   B.hPut handle "\n\n\n"
   drain
   hClose handle
+
+-- |Some or even all USB serial devices have no support for XON/XOFF
+-- and they have so large output buffers which make proper XON/XOFF
+-- impossible even though Linux supports it. This function implements
+-- quick and dirty software handshake.
+putXonXoff :: Serial -> B.ByteString -> IO ()
+putXonXoff s@Serial{..} bs = do
+  B.hPut handle now
+  drain
+  status <- B.hGetNonBlocking handle 1
+  when (status == "\DC3") $ do
+    putStrLn "STOPPED"
+    _ <- B.hGet handle 1
+    putStrLn "RESUMED"
+  if B.null later
+    then return ()
+    else putXonXoff s later
+  where (now, later) = B.splitAt 128 bs
